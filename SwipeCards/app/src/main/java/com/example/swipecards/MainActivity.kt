f17f43b85e9e3d665f8b962342f83333967b1641
Чибирев Александр Дмитриевич <ADChibirev@sberbank.ru>
2021-03-29 19:16:31 +0300

    swipe cards with rv

---
+++ SwipeCards/app/src/main/java/com/example/swipecards/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.swipecards
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.start)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = AdapterN()
+    }
+}
+
+class AdapterN : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_card, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return 6
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        holder.itemView.background = ContextCompat.getDrawable(holder.itemView.context, R.drawable.card_background)
+    }
+}
+
+class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
